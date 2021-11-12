41977e22cf638edb126e1b7f0ce8f9772016772b
Bálint <hidden@somewhere.org>
2021-11-12 14:47:22 +0100

    new and delete

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
+
+
+using namespace std;
 
 int main()
 {
-	if (int* pointerToHeap = (int*)malloc(sizeof(int)))
-	{
-		*pointerToHeap = 12;
-		printf("%d", *pointerToHeap);
-		free(pointerToHeap);
-	}
+	int* pointerToHeap = new int(5);
+	cout << *pointerToHeap << '\n';
+	delete pointerToHeap;
 
-	int* arrayOfFive = (int*)malloc(5 * sizeof(int));
-	printf("Allocated at: %p", arrayOfFive);
-	arrayOfFive[0] = 1;
-	arrayOfFive[1] = 2;
-	//arrayOfFive[5] = 213;	// DANGEROUS! INVALID! (OUT OF BOUNDS)
-	free(arrayOfFive);
+	int* pointerToArray = new int[10]{ 2, 3, 4 };
+	cout << pointerToArray[0] << pointerToArray[1] << pointerToArray[8] << '\n';
+	delete[] pointerToArray;
+
+	/*int** pointerToArrayOfArray = new int* [5];
+	for (int i = 0; i < 5; ++i)
+		pointerToArrayOfArray[i] = new int[10];*/
 }
