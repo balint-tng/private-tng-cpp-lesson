0518c78042abdc0f335ff6f60f50b21b145e6f3f
Bálint <hidden@somewhere.org>
2021-11-12 10:17:26 +0100

    Literals and operators

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <complex>
+#include <chrono>
 
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!\n";
+	// ComplexNumber a = new ComplexNumber(3.13, 2);
+	// ComplexNumber b = new ComplexNumber(3.13, 2);
+	// ComplexNumber r = a.mulitply(b);
+	complex a = 3.13 + 2i;
+	complex a = 3.13 + 2i;
+	complex r = a * a;
+
+	13s; 2ms;
+
+	int i;
 }
