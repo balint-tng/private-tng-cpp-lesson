5e270876edb7084c698722f7b60e9eb46a3aa9bd
Bálint <hidden@somewhere.org>
2021-11-12 10:32:24 +0100

    Macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-
+//#define min(a, b) ((a) < (b))
 
 using namespace std;
-
 
 void function(int a, int b = 0, int c = 0)
 {
@@ -12,10 +11,20 @@ void function(int a, int b = 0, int c = 0)
 void overload(int) {}
 void overload(char) {}
 
+/*int min(int a, int b)
+{}*/
+
 int main()
 {
 	function(12, 3);
 	function(1, 2, 3);
 	overload(21);
 	overload('a');
+	//std::min()
 }
+
+/*
+	1. Preprocessor
+	2. Compilation
+	3. Linker
+*/
