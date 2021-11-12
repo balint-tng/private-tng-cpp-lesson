3d0e5289cbf69ab1c3cce3a6109cfdeffb73c084
Bálint <hidden@somewhere.org>
2021-11-12 10:21:21 +0100

    Default arguments and overloads.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <complex>
-#include <chrono>
 
 
 using namespace std;
 
+
+void function(int a, int b = 0, int c = 0)
+{
+	cout << a << '\n';
+}
+
+void overload(int) {}
+void overload(char) {}
+
 int main()
 {
-	// ComplexNumber a = new ComplexNumber(3.13, 2);
-	// ComplexNumber b = new ComplexNumber(3.13, 2);
-	// ComplexNumber r = a.mulitply(b);
-	complex a = 3.13 + 2i;
-	complex a = 3.13 + 2i;
-	complex r = a * a;
-
-	13s; 2ms;
-
-	int i;
+	function(12, 3);
+	function(1, 2, 3);
+	overload(21);
+	overload('a');
 }
