45946da7503a3a9304acd5ded462fdf86d2757c5
Bálint <hidden@somewhere.org>
2021-11-12 10:53:07 +0100

    Data types

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 //#define min(a, b) ((a) < (b))
 
 using namespace std;
@@ -28,3 +29,30 @@ int main()
 	2. Compilation
 	3. Linker
 */
+
+void dataTypes()
+{
+	// uint32_t;
+
+	char a = 'q';	// 1 byte
+	char a2 = 1;
+	short b = 12;	// 2 bytes (here)
+	int c;			// 4 bytes (here)
+	long d;			// 4 bytes (here)
+	long long e;	// 8 bytes
+	signed char f;
+	unsigned char g;
+	unsigned int h;
+
+	float i = 3.14f;
+	double k = 3.14;
+	long double l;
+
+	const char string1[] = "string";
+	const char* string2 = "string";
+
+	string stdstring = "some string";
+
+	int array[3];
+	int initializedArray[/*3*/] = {1, 2, 3};
+}
