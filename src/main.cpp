dafba8d32ea3e313aff4d33d6bb6bbb4ba6647b7
Ismail <>
2021-11-12 16:17:05 +0100

    Lifecycle functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,97 @@
 #include <iostream>
-
-
+#include <stdexcept>
 using namespace std;
-
-struct Position
+class Array
 {
-	int x;
-	int y;
-};
-
-class BigAssObject
-{
-public:
-	int someConstFunction() const { return 0; }
-	int someNonConstFunction() { return 0; }
-};
-
-class GameObject
-{
-public:
-	void setPosition(Position position) /*const*/ { myPosition = position; }
-	Position getPosition() const { return myPosition; }
-
-	void setBAO(BigAssObject bao) { myBAO = bao; /*Should use move!*/ }
-	const BigAssObject& getBAO() const { return myBAO; }
-	BigAssObject& getBAO() { return myBAO; }
-
 private:
-	Position myPosition;
-	BigAssObject myBAO;
+	unsigned mySize;
+	int* array;
+public:
+	/* Array()
+	{
+		mySize = 0;
+		array = nullptr;
+	} */
+	Array(unsigned size) : mySize(size)
+	{
+		if (size == 0)
+			throw runtime_error("Size may not be zero!");
+		array = new int[mySize]();
+	}
+
+	Array(const Array& otherArray)
+	{
+		mySize = otherArray.mySize;
+		array = new int[mySize];
+		for (int i = 0; i < mySize; ++i)
+		{
+			array[i] = otherArray.array[i];
+		}
+	}
+	Array(Array&& otherArray)
+	{
+		mySize = otherArray.mySize;
+		array = otherArray.array;
+		otherArray.array = nullptr;
+	}
+	Array& operator=(Array&& otherArray)
+	{
+		mySize = otherArray.mySize;
+		array = otherArray.array;
+		otherArray.array = nullptr;
+		return *this;
+	}
+	Array& operator=(const Array& otherArray)
+	{
+		mySize = otherArray.mySize;
+		delete[] array;
+		array = new int[mySize];
+		for (int i = 0; i < mySize; ++i)
+		{
+			array[i] = otherArray.array[i];
+		}
+		return *this;
+	}
+	~Array()
+	{
+		cout << "delete", delete[] array, cout << "dont do this";
+	}
+	int getArrayElement(unsigned position) const
+	{
+		if (position < mySize)
+			return array[position];
+		throw runtime_error("out of bounds");
+	}
+	void setArrayElementAtPosition(int value, int position)
+	{
+		if (position >= mySize)
+			throw runtime_error("out of bounds");
+		array[position] = value;
+	}
+
 };
 
-void certainPartOfTheCode(const GameObject& object)
+void f(Array array)
 {
-	object.getBAO().someConstFunction();
-}
-
-void otherPlace(GameObject& object)
-{
-	object.getBAO().someNonConstFunction();
+	cout << array.getArrayElement(2) << '\n';
 }
 
 int main()
 {
-	Position p;
-	p.x = 1;
-	p.y = 2;
-
-	const GameObject object;
-	//object.setPosition(p);
-	//object.setPosition({ 1, 2 });
-	Position p2 = object.getPosition();
-
-	const GameObject& refToObject = object;
-	//refToObject.setPosition({ 1, 2 });
+	try
+	{
+		Array array(3);
+		array.setArrayElementAtPosition(10, 1);
+		array.getArrayElement(1);
+		//cout << array.getArrayElement(0) << " " << array.getArrayElement(1) << " " << array.getArrayElement(2) << endl;
+		f(array);
+		cout << "hello";
+		//cin.get();
+		Array array2(4);
+		array = array2;
+	}
+	catch (const exception& e)
+	{
+		cerr << e.what();
+	}
 }
